009ae5b06ef8b00cb5df926a3b9734a811964d7f
Franco Garnica <franco.garnica@yahoo.com>
2023-06-22 04:52:15 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent
+    //AnotherComponent1
+    //ETC
+  ], //AppComponent pertenece a AppModule
   imports: [
     BrowserModule,
     AppRoutingModule
